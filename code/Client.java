2bdbdf58b95d9f71b150071d96ca8ac95790ab16
Tiago Teixeira <101202571+T-yago@users.noreply.github.com>
2023-11-19 15:49:15 +0000

    Fixed neste try catch

--- code/Client.java
+++ code/Client.java
@@ -9,76 +9,57 @@ import java.io.BufferedReader;
 public class Client {
     public static void main(String[] args) {
         try {
-            Socket s = new Socket("localhost", 22345);
+            Socket s = new Socket("localhost", 22347);
+            Connection conn = new Connection(s);
+            int option = -1;
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 
-            try {
+            while (option == -1) {
+                System.out.print("\n//////////// LOGIN/REGISTO /////////////\n"
+                        + "\n1) Registar nova conta.\n"
+                        + "2) Iniciar sessão.\n"
+                        + "\n");
 
-                Connection conn = new Connection(s);
-
-                int option = -1;
-                BufferedReader stdin = new BufferedReader(new InputStreamReader (System.in));
-
-                while (option == -1) {
-                    System.out.print("\n//////////// LOGIN/REGISTO /////////////\n"
-                            + "\n1) Registar nova conta.\n"
-                            + "2) Iniciar sessão.\n"
-                            + "\n");
-
-                    try{
-                        option = Integer.parseInt(stdin.readLine().trim());
-                    } catch (NumberFormatException e) {
-                        System.out.println("Invalid input. Please enter a valid integer.");
-                        option = -1;
-                    }
+                try {
+                    option = Integer.parseInt(stdin.readLine().trim());
 
                     if (option == 1) {
                         System.out.print("\n////////////REGISTAR NOVA CONTA/////////////\n"
                                 + "\n"
                                 + "Introduza o seu username: ");
-                        try {
-                            String username = stdin.readLine();
+                        String username = stdin.readLine();
 
-                            System.out.print("\nIntroduza a sua password: ");
-                                String password = stdin.readLine();
-                                Account acc = new Account(username, password);
-                                System.out.println("Invalid input. Please enter a valid string for the password.");
+                        System.out.print("\nIntroduza a sua password: ");
+                        String password = stdin.readLine();
+                        Account acc = new Account(username, password);
+                        Message message = new Message(0, acc);
+                        conn.send(message);
 
-                        } catch (IOException e) {
-                            System.out.println("Invalid input. Please enter a valid string for the username.");
-                            option = -1;
-                        }
-    
-                    } if (option == 2) {
+                    } else if (option == 2) {
                         System.out.print("\n////////////INICIAR SESSÃO/////////////\n"
                                 + "\n"
                                 + "Introduza o seu username: ");
-                        try {
-                            String username = stdin.readLine();
+                        String username = stdin.readLine();
 
-                            System.out.print("\nIntroduza a sua password: ");
-                            String password = stdin.readLine();
-                            Account acc = new Account(username, password);
-                            Message message = new Message(0, acc);
-
-                            conn.send(message);
-                        } catch (IOException e) {
-                            System.out.println("Invalid input. Please enter a valid string.");
-                            option = -1;
-                        }
-
-                    }
-                    else {
+                        System.out.print("\nIntroduza a sua password: ");
+                        String password = stdin.readLine();
+                        Account acc = new Account(username, password);
+                        Message message = new Message(1, acc);
+                        conn.send(message);
+                        
+                    } else {
                         System.out.println("Please input an integer that corresponds to one of the options.");
                         option = -1;
                     }
+                } catch (NumberFormatException | IOException e) {
+                    System.out.println("Invalid input. Please enter a valid integer or string.");
+                    option = -1;
                 }
-            } catch (IOException e) {
-                System.out.println("Error creating connection, exiting program");
-                System.exit(1);
             }
         } catch (IOException e) {
-            System.out.println("Connection refused, exiting program");
+            System.out.println("Error creating connection or connection refused, exiting program");
             System.exit(1);
         }
     }
 }
+
